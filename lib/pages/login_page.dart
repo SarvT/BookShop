527e3d019272b2361a23b0081cedcf4b043a3b08
SarvT <91484197+SarvT@users.noreply.github.com>
2023-02-20 22:24:59 +0530

    day 8 done. all the valditions and logic for login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,104 +11,136 @@ class LoginApp extends StatefulWidget {
   State<LoginApp> createState() => _LoginAppState();
 }
 
-String name = "";
-bool changeBtn = false;
-
 class _LoginAppState extends State<LoginApp> {
+  String name = "";
+  bool changeBtn = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext buildContext) async {
+    if (_formKey.currentState!.validate()) {
+      
+    setState(() {
+      changeBtn = true;
+    });
+    await Future.delayed(const Duration(milliseconds: 500));
+    await Navigator.pushNamed(context, MyRoutes.homeRoute);
+    setState(() {
+      changeBtn = false;
+    });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.lightBlue,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'asset/images/Project_69-01.jpg',
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 20,
-              width: 20,
-              // child: Text("data"),
-            ),
-            Text(
-              "Welcome $name",
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'asset/images/Project_69-01.jpg',
+                fit: BoxFit.cover,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-              width: 20,
-              // child: Text("data"),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
-              child: Column(children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Enter UNAME", labelText: "UNAME"),
-                  onChanged: (value) => {
-                    name = value,
-                    setState(() {}),
-                  },
+              const SizedBox(
+                height: 20,
+                width: 20,
+                // child: Text("data"),
+              ),
+              Text(
+                "Welcome $name",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      hintText: "Enter pswd", labelText: "PSWD"),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-
-                InkWell(
-                  onTap: () async {
-                    setState(() {
-                      changeBtn = true;
-                    });
-                    await Future.delayed(const Duration(milliseconds: 500));
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 500),
-                    width: changeBtn ? 50 : 130,
-                    height: 40,
-                    alignment: Alignment.center,
-                    child: changeBtn
-                        ? const Icon(
-                            Icons.done,
-                            color: Colors.white,
-                          )
-                        : const Text(
-                            "Submit",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                    decoration: BoxDecoration(
-                        color: Colors.deepPurpleAccent,
-                        // shape: changeBtn? BoxShape.circle:BoxShape.rectangle,
-                        borderRadius:
-                            BorderRadius.circular(changeBtn ? 50 : 6)),
+              ),
+              const SizedBox(
+                height: 20,
+                width: 20,
+                // child: Text("data"),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter UNAME", 
+                        labelText: "UNAME"
+                        ),
+                        validator: (value) {
+                      if (value!.isEmpty) {
+                        return "UNAME can't be empty";
+                      }
+                      // return null;
+                    },
+                    onChanged: (value) => {
+                      name = value,
+                      setState(() {}),
+                    },
                   ),
-                )
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter pswd", labelText: "PSWD"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "pswd can't be empty";
+                      }
+                      else if (value.length < 8) {
+                        return "pswd can't be less than 8 chars";
+                      }
+                        },
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
 
-                // ElevatedButton(
-                //   onPressed: () {
-                //     // print("naah don't!");
-                //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                //   },
-                //   child: const Text("SUBMIT"),
-                //   style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
-                // )
-              ]),
-            )
-          ],
+                  Material(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(changeBtn ? 50 : 6),
+                    child: InkWell(
+                      onTap: () => moveToHome(context),
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 500),
+                        width: changeBtn ? 50 : 130,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: changeBtn
+                            ? const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : const Text(
+                                "Submit",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                        // decoration: BoxDecoration(
+                        //     color: Colors.deepPurpleAccent,
+                        // )
+                        // shape: changeBtn? BoxShape.circle:BoxShape.rectangle,
+                      ),
+                    ),
+                  )
+
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     // print("naah don't!");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   child: const Text("SUBMIT"),
+                  //   style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                  // )
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
