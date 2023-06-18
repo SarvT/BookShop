30f41cc865124984753da5d7ac25505a95c1f77f
SarvT <91484197+SarvT@users.noreply.github.com>
2023-06-18 17:10:34 +0530

    It's a start now. day first commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:oneflut/models/items.dart';
 import 'package:oneflut/widgets/drawer.dart';
 import 'package:oneflut/widgets/item_widget.dart';
+import 'package:oneflut/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomeApp extends StatefulWidget {
   const HomeApp({Key? key}) : super(key: key);
@@ -33,31 +35,126 @@ class _HomeAppState extends State<HomeApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData(color: Colors.black),
-          backgroundColor: Colors.white,
-          title: const Text(
-            "Swarn",
-            style: TextStyle(color: Colors.black),
-          ),
+    // return MaterialApp(
+    // home: Scaffold(
+    // appBar: AppBar(
+    //   iconTheme: const IconThemeData(color: Colors.black),
+    //   backgroundColor: Colors.white,
+    //   title: const Text(
+    //     "Swarn",
+    //     style: TextStyle(color: Colors.black),
+    //   ),
+    // ),
+    // body:
+    // Padding(
+    //     padding: EdgeInsets.all(16),
+    //     child: (CatalogueModel.items != null &&
+    //             CatalogueModel.items.isNotEmpty)
+    //         // ? ListView.builder(
+    //         //     itemCount: CatalogueModel.items.length,
+    //         //     itemBuilder: (context, index) => ItemWidget(
+    //         //       item: CatalogueModel.items[index],
+    //         //     ),
+    //         //   )
+    //         ? GridView.builder(
+    //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //                 crossAxisCount: 2,
+    //                 mainAxisSpacing: 16,
+    //                 crossAxisSpacing: 16),
+    //             itemBuilder: (context, index) {
+    //               final item = CatalogueModel.items[index];
+    //               return Card(
+    //                 clipBehavior: Clip.antiAlias,
+    //                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    //                 child: GridTile(
+    //                   header: Container(
+    //                     padding: EdgeInsets.all(12),
+    //                     child: Text(item.name,
+    //                     style: const TextStyle(
+    //                       color: Colors.white
+    //                     ),),
+    //                     decoration: const BoxDecoration(
+    //                       color: Colors.amber,
+
+    //                     ),),
+    //                   child: Image.network(item.img),
+    //                   footer: Text(item.price.toString()),
+    //                   )
+    //                 );
+    //             },
+    //             itemCount: CatalogueModel.items.length,
+    //           )
+    //         : const Center(
+    //             child: CircularProgressIndicator(),
+    //           )),
+    // drawer: const MyDrawer(),
+
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: Vx.m32,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CatalogueHeader(),
+            if (CatalogueModel.items != null && CatalogueModel.items.isNotEmpty)
+              Expanded(child: CatalogueList())
+            else
+              Center(
+                child: CircularProgressIndicator(),
+              )
+          ],
         ),
-        body: Padding(
-            padding: EdgeInsets.all(16),
-            child: (CatalogueModel.items != null &&
-                    CatalogueModel.items.isNotEmpty)
-                ? ListView.builder(
-                    itemCount: CatalogueModel.items.length,
-                    itemBuilder: (context, index) => ItemWidget(
-                      item: CatalogueModel.items[index],
-                    ),
-                  )
-                : Center(
-                    child: CircularProgressIndicator(),
-                  )),
-        drawer: const MyDrawer(),
       ),
+    ));
+  }
+}
+
+class CatalogueHeader extends StatelessWidget {
+  // const CatalogueHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Mr.Store".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        Text("Mr.Store"),
+      ],
     );
   }
+}
+
+class CatalogueList extends StatelessWidget {
+  const CatalogueList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final catalog = CatalogueModel.items[index];
+        return CatalogueItem(catalog: catalog);
+      },
+      itemCount: CatalogueModel.items.length,
+    );
+  }
+}
+
+class CatalogueItem extends StatelessWidget {
+  final catalog;
+  CatalogueItem({Key? key, @required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+// final Item catalog = catalog;
+
+  @override
+  Widget build(BuildContext context) {
+    // var catalog;
+    return VxBox(
+        child: Row(
+      children: [Image.network(catalog.Image)],
+    )).white.roundedLg.square(100).make().py16();
+  }
+
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData( 
       primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -15,4 +17,8 @@ class MyTheme {
       static ThemeData darkTheme(BuildContext context) => ThemeData( 
         brightness: Brightness.dark
       );
+
+      // colors
+      static Color creamColor = Color(0xfff5f5f5); 
+      static Color darkBluishColor = Color(0xff403b58); 
 }
